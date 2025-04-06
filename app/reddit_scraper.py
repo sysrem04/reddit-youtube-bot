81c4d353dcfda7a65620dd99784313ed8d44c3dd
sysrem04 <50791235+sysrem04@users.noreply.github.com>
2025-04-06 23:00:29 +0700

    Update reddit_scraper.py

--- app/reddit_scraper.py
+++ app/reddit_scraper.py
@@ -4,10 +4,12 @@ import os
 import uuid
 
 def fetch_reddit_video():
-    reddit = praw.Reddit(
-        client_id=os.getenv("REDDIT_CLIENT_ID"),
-        client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-        user_agent="script:reddit.youtube:v1.0 (by u/YOUR_USERNAME)"
+reddit = praw.Reddit(
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+    user_agent=os.getenv("REDDIT_USER_AGENT")
+)
+
     )
 
     subreddit = reddit.subreddit("kactress")
