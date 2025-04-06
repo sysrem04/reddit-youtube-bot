e7207eaec5bb26c0439a47000a64e2feb735767d
sysrem04 <50791235+sysrem04@users.noreply.github.com>
2025-04-06 22:35:42 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
+from app.reddit_scraper import fetch_reddit_video
+from app.youtube_uploader import authenticate_youtube, upload_video
+from app.music_adder import add_background_music
+
+def main():
+    print("📥 Fetching videos from Reddit...")
+    title, video_path = fetch_reddit_video()
+
+    print(f"🎬 Using original Reddit video: {title}")
+    final_video_path = add_background_music(video_path)
+
+    print("🔐 Authenticating YouTube...")
+    youtube = authenticate_youtube()
+
+    print("📤 Uploading...")
+    upload_video(youtube, final_video_path, title, title)
+
+if __name__ == "__main__":
+    main()
 
